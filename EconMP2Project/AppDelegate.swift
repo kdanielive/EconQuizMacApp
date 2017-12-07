17caeae837ded7d6692992139478b0bfd5f3db8f
Daniel Kim <danielkim@daniels-mbp.hotchkiss.net>
2017-12-06 20:15:46 -0500

    Initial Commit

---
+++ EconMP2Project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EconMP2Project
+//
+//  Created by Daniel Kim on 12/6/17.
+//  Copyright © 2017 Daniel Kim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
